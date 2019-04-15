4be522b2c364f0f50fe93e3242d63d48ba419433
Gunay <lambertalv@gmail.com>
2019-04-15 20:28:03 +0700

    Черновик

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,9 @@ var menuBtn = document.querySelector('.nav__toggle');
 var menu = document.querySelector('.nav__container');
 var headerTop = document.querySelector('.page-header__top');
 
+headerTop.classList.remove('page-header__top--no-js');
+menu.classList.remove('nav__container--no-js');
+
 menuBtn.addEventListener('click', function() {
   menu.classList.toggle('nav__container--opened');
   this.classList.toggle('nav__toggle--opened');
