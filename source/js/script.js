febbe95b22ebe6cdc9cb7f34d87d3e9e106c7368
Gunay <lambertalv@gmail.com>
2019-04-18 16:21:36 +0700

    Верстка блока reviews

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,3 @@
-// var menuBtn = document.querySelector('.nav__toggle');
-// var menu = document.querySelector('.nav__container');
-// var headerTop = document.querySelector('.page-header__top');
-
-// headerTop.classList.remove('page-header__top--no-js');
-// menu.classList.remove('nav__container--no-js');
-
-// menuBtn.addEventListener('click', function() {
-//   menu.classList.toggle('nav__container--opened');
-//   this.classList.toggle('nav__toggle--opened');
-//   headerTop.classList.toggle('page-header__top--opened');
-// })
-
 var header = document.querySelector('.page-header');
 var menuBtn = document.querySelector('.page-header__toggle');
 var menu = document.querySelector('.nav');
@@ -25,4 +12,47 @@ menuBtn.addEventListener('click', function() {
   this.classList.toggle('toggle__button--opened');
   menu.classList.toggle('nav--opened');
   headerBg.classList.toggle('page-header__bg--opened');
-})
+});
+
+var reviewItems = document.querySelectorAll('.reviews__item');
+var prevBtn = document.querySelector('.reviews__button--prev');
+var nextBtn = document.querySelector('.reviews__button--next');
+var i = 0;
+
+prevBtn.addEventListener('click', function() {
+  reviewItems[i].classList.remove('reviews__item--shown');
+  i--;
+  if (i < 0) {
+    i = reviewItems.length - 1;
+  }
+  reviewItems[i].classList.add('reviews__item--shown');
+});
+
+nextBtn.addEventListener('click', function() {
+  reviewItems[i].classList.remove('reviews__item--shown');
+  i++;
+  if (i >= reviewItems.length) {
+    i = 0;
+  }
+  reviewItems[i].classList.add('reviews__item--shown');
+});
+
+var sliderItems = document.querySelectorAll('.slider__item');
+
+sliderItems[0].addEventListener('click', function() {
+  slider(reviewItems[1], reviewItems[2], reviewItems[0]);
+});
+
+sliderItems[1].addEventListener('click', function() {
+  slider(reviewItems[0], reviewItems[2], reviewItems[1]);
+});
+
+sliderItems[2].addEventListener('click', function() {
+  slider(reviewItems[1], reviewItems[0], reviewItems[2]);
+});
+
+function slider(hiddenSlide1, hiddenSlide2, shownSlide) {
+  hiddenSlide1.classList.remove('reviews__item--shown');
+  hiddenSlide2.classList.remove('reviews__item--shown');
+  shownSlide.classList.add('reviews__item--shown');
+}
